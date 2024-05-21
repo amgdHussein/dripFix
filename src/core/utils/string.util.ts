14ba1d0a423562a94a465533e151ee4bf0a3a211
Amgad Hussein <amgadhussain.iqranetwork@gmail.com>
2024-05-21 12:24:38 +0400

    add string utils

---
+++ src/core/utils/string.util.ts
@@ -0,0 +1,13 @@
+export namespace StringUtils {
+  /**
+   * Capitalize a string
+   * @param { string } str string to capitalize
+   * @return { string } capitalized string
+   * @example
+   *  const name = capitalize('amgad');
+   *    name => 'Amgad'
+   */
+  export function capitalize(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+}
