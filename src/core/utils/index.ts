f23f2ac213c2bab66df6a160ecfd6aee0ccd5c23
Amgad Hussein <amgadhussain.iqranetwork@gmail.com>
2024-05-21 12:23:18 +0400

    make prettier default typescript formatter

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,1 +1,7 @@
-export * from './object.util';
+import * as ObjectUtils from './object.util';
+import * as StringUtils from './string.util';
+
+export namespace Utils {
+  export import Object = ObjectUtils;
+  export import String = StringUtils;
+}
--- src/core/utils/object.util.ts
+++ src/core/utils/object.util.ts
@@ -1,91 +1,89 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export namespace ObjectUtils {
-  /**
-   * Flatten the object and convert the nested properties into dot notation keys.
-   * @param { object } obj Object to convert it into dot notation keys
-   * @return { object } The flatten object
-   * @example
-   *  const initialData = {
-   *    name: 'Frank',
-   *    age: 12,
-   *    favorites: {
-   *      food: 'Pizza',
-   *      color: 'Blue',
-   *      subject: 'recess'
-   *    }
-   *  };
-   *
-   *  const flattenedData = flattenObject(initialData);
-   *    flattenedData => {
-   *      name: 'Frank',
-   *      age: 12,
-   *      'favorites.food': 'Pizza',
-   *      'favorites.color': 'Blue',
-   *      'favorites.subject': 'recess'
-   *    }
-   */
-  export function flatten(obj: any, parentKey = ''): any {
-    return Object.keys(obj).reduce((acc, key) => {
-      const prefixedKey = parentKey ? `${parentKey}.${key}` : key;
-      if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
-        Object.assign(acc, flatten(obj[key], prefixedKey));
-      } else {
-        acc[prefixedKey] = obj[key];
-      }
-      return acc;
-    }, {});
+/**
+ * Flatten the object and convert the nested properties into dot notation keys.
+ * @param { object } obj Object to convert it into dot notation keys
+ * @return { object } The flatten object
+ * @example
+ *  const initialData = {
+ *    name: 'Frank',
+ *    age: 12,
+ *    favorites: {
+ *      food: 'Pizza',
+ *      color: 'Blue',
+ *      subject: 'recess'
+ *    }
+ *  };
+ *
+ *  const flattenedData = flattenObject(initialData);
+ *    flattenedData => {
+ *      name: 'Frank',
+ *      age: 12,
+ *      'favorites.food': 'Pizza',
+ *      'favorites.color': 'Blue',
+ *      'favorites.subject': 'recess'
+ *    }
+ */
+export function flat(obj: any, parentKey = ''): any {
+  return Object.keys(obj).reduce((acc, key) => {
+    const prefixedKey = parentKey ? `${parentKey}.${key}` : key;
+    if (typeof obj[key] === 'object' && obj[key] !== null && !Array.isArray(obj[key])) {
+      Object.assign(acc, flat(obj[key], prefixedKey));
+    } else {
+      acc[prefixedKey] = obj[key];
+    }
+    return acc;
+  }, {});
+}
+
+/**
+ * Drop the undefined fields from an object.
+ * @param { object } obj Object to drop undefined sub-fields
+ * @return { object } The valid object
+ * @example
+ *  const obj = {
+ *    name: 'Frank',
+ *    age: 12,
+ *    favorites: {
+ *      food: 'Pizza',
+ *      colors: [undefined, 'Blue'],
+ *      subject: undefined
+ *    }
+ *  };
+ *
+ *  const validObj = dropUndefined(obj);
+ *    validObj => {
+ *      name: 'Frank',
+ *      age: 12,
+ *      favorites: { food: 'Pizza', colors: [ 'Blue' ] }
+ *    }
+ */
+export function dropUndefined(obj: any): any {
+  if (obj === null || obj === undefined) {
+    return undefined;
   }
 
-  /**
-   * Drop the undefined fields from an object.
-   * @param { object } obj Object to drop undefined sub-fields
-   * @return { object } The valid object
-   * @example
-   *  const obj = {
-   *    name: 'Frank',
-   *    age: 12,
-   *    favorites: {
-   *      food: 'Pizza',
-   *      colors: [undefined, 'Blue'],
-   *      subject: undefined
-   *    }
-   *  };
-   *
-   *  const validObj = dropUndefined(obj);
-   *    validObj => {
-   *      name: 'Frank',
-   *      age: 12,
-   *      favorites: { food: 'Pizza', colors: [ 'Blue' ] }
-   *    }
-   */
-  export function dropUndefined(obj: any): any {
-    if (obj === null || obj === undefined) {
-      return undefined;
-    }
-
-    if (Array.isArray(obj)) {
-      const newArray = obj.map(item => dropUndefined(item)).filter(item => item !== undefined);
-      return newArray.length > 0 ? newArray : undefined;
-    }
-
-    if (typeof obj === 'object') {
-      const targetObj: Record<string, any> = {};
-
-      for (const key in obj) {
-        const value = dropUndefined(obj[key]);
-        if (value !== undefined) {
-          targetObj[key] = value;
-        }
-      }
-
-      return Object.keys(targetObj).length > 0 ? targetObj : undefined;
-    }
-
-    if (typeof obj === 'string' && obj.trim() === '') {
-      return undefined;
-    }
-
-    return obj;
+  if (Array.isArray(obj)) {
+    const newArray = obj.map(item => dropUndefined(item)).filter(item => item !== undefined);
+    return newArray.length > 0 ? newArray : undefined;
   }
+
+  if (typeof obj === 'object') {
+    const targetObj: Record<string, any> = {};
+
+    for (const key in obj) {
+      const value = dropUndefined(obj[key]);
+      if (value !== undefined) {
+        targetObj[key] = value;
+      }
+    }
+
+    return Object.keys(targetObj).length > 0 ? targetObj : undefined;
+  }
+
+  if (typeof obj === 'string' && obj.trim() === '') {
+    return undefined;
+  }
+
+  return obj;
 }
